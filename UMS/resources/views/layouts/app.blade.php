0906e426b8fb51292fba562b9c62f82d3d14ab76
shubham <swardshub@gmail.com>
2021-06-10 23:27:10 +0530

    form

--- UMS/resources/views/layouts/app.blade.php
+++ UMS/resources/views/layouts/app.blade.php
@@ -18,6 +18,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.css">
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
@@ -83,6 +86,26 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
     
+    
+    <script type="text/javascript">
+        $(function() {
+            $('#datetimepicker1').datetimepicker();
+        });
+</script>
+
+    <script type="text/javascript">
+        $(function() {
+            $('#datetimepicker2').datetimepicker();
+        });
+</script>
+
+@yield('scripts')
+
 </body>
 </html>
--- UMS/resources/views/user/create.blade.php
+++ UMS/resources/views/user/create.blade.php
@@ -2,28 +2,92 @@
 @section('content')
 
 <div class="container">
-        <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-             <label for="Name">Full Name</label>
-             <input type="text" class="form-control" placeholder="Name">
-        </div>
-        <div class="form-group">
-                        <label for="featured">Featured Image</label>
-                        <input type="file" name="featured" class="form-control">
-        </div>
+       
+    <div class="card card-default">
+       
+        <div class="card-body">
+            
+            <form class="px-4 py-3">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                            <div class="form-group">
+                                <label for="Name">Full Name</label>
+                                <input type="text" class="form-control" placeholder="Name">
+                            </div>
+                            <div class="form-group">
+                                            <label for="featured">Featured Image</label>
+                                            <input type="file" name="featured" class="form-control">
+                            </div>
         <!-- <div class="form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
             <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div> -->
+
+                            <div class="form-group row">
+                                    <div >
+                                            <label for="Date of joining">Date of Joining</label>
+                                            <section class="col-sm-10 ">
+                                                <div class="form-group">
+                                                    <div class='input-group date' id='datetimepicker1'>
+                                                        <input type='text' class="form-control" />
+                                                        <span class="input-group-addon">
+                                                                <span class="glyphicon glyphicon-calendar"></span>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </section>
+                                    </div>
+                            
+                                    <div>
+                                            <label for="Date of leaving ">Date of leaving</label>
+                                            <section class="col-sm-10">
+                                                <div class="form-group">
+                                                    <div class='input-group date' id='datetimepicker2'>
+                                                        <input type='text' class="form-control" />
+                                                        <span class="input-group-addon">
+                                                                <span class="glyphicon glyphicon-calendar"></span>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </section>
+                                    </div>  
+                            </div>
+
+                            <div class="form-group col-sm-12">
+                                    <div >
+                                                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
+                                                    <label class="form-check-label" for="flexCheckChecked">&nbsp&nbsp&nbsp&nbsp Still Working</label>
+                                                    </div>                                   
+                                    </div>
         
-        <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+                            <div>
+                                    <button onclick="calculate()" type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+            </form>
+            
+        </div>
+
+    </div>
 
 </div>
+@endsection
+
+
+@section('scripts')
+
+        <script>
+                const calculate=()=>{
+                    let join=document.getElementById("datetimepicker1").value;
+                    let leave=document.getElementById("datetimepicker2").value;
+
+                    let difference=join-leave;
+
+                    console.log(difference);
+                }
+        </script>
 
 @endsection
+
